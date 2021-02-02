26dda0d4f039db4bd673f4c6a76517dcaff26ad6
WaledSalem <eng.walied.salem.capw@gmail.com>
2021-02-02 01:57:18 +0000

    flask6

--- app.py
+++ app.py
@@ -23,6 +23,18 @@ class Person(db.Model):
     height = db.Column(db.Float)
 
 
+class Countries(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    cities = db.relationship('Cities', backref='country')
+
+
+class Cities(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    country_id = db.Column(db.Integer, db.ForeignKey('countries.id'), nullable=False)
+
+
 @app.route('/')
 def hello_internet():
     return "Hello Internet!"
--- create.py
+++ create.py
@@ -1,8 +1,19 @@
-from app import db, Users, Person
+from app import db, Users, Countries, Cities
 
 db.drop_all()
 db.create_all()
 
 test_user = Users(first_name='Groot', last_name='Toot')
 db.session.add(test_user)
+db.session.commit()
+
+UK = Countries(name='United Kingdom')
+db.session.add(UK)
+db.session.commit()
+
+ldn = Cities(name='London', country=UK)
+mcr = Cities(name='Manchester', country=Countries.query.filter_by(name='United Kingdom').first())
+
+db.session.add(ldn)
+db.session.add(mcr)
 db.session.commit()
